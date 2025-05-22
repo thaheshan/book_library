500fa89c4925322f7b29069a5c0394757c72c540
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2025-05-22 14:55:53 +0530

    update main file
    udated

--- my-book-app/src/main.ts
+++ my-book-app/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
