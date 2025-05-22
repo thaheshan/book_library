e43a7be57e7e3ea5f85f0b82eb7b9c32a5287136
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2025-05-22 23:11:19 +0530

    updated app routes
    updated app routing for the proper files structures with proper pathway in order for navigation from the home page

--- my-book-app/src/app/app.routes.ts
+++ my-book-app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { 
+    path: 'books', 
+    loadComponent: () => import('./books/book-list/book-list.component').then(m => m.BookListComponent) 
+  },
+  { 
+    path: 'books/new', 
+    loadComponent: () => import('./books/book-form/book-form.component').then(m => m.BookFormComponent) 
+  },
+  { 
+    path: 'books/:id', 
+    loadComponent: () => import('./books/book-detail/book-detail.component').then(m => m.BookDetailComponent) 
+  },
+  { 
+    path: 'books/:id/edit', 
+    loadComponent: () => import('./books/book-form/book-form.component').then(m => m.BookFormComponent) 
+  },
+  { 
+    path: '**', 
+    loadComponent: () => import('./core/not-found/not-found.component').then(m => m.NotFoundComponent) 
+  }
+];
